1e4a4992e96b642ee60319f98b38a9858222c54e
archen <aqssxlzc@gmail.com>
2014-05-12 23:28:52 +0800

    add func

--- updater.py
+++ updater.py
@@ -21,82 +21,85 @@ import logging
 import os
 import re
 
-#1.get local goagent path
-localpath="C:\\Users\\Administrator"
-logging.warning('goagent path is '+localpath)
-goagentPath=""
-goagentFolderPattern = "goagent"
-pattern=re.compile(goagentFolderPattern)
-for filename in os.listdir(localpath):
-	if pattern.match(filename)!=None:
-		goagentPath=localpath+"\\"+filename
-		print  goagentPath
-    
-#2.check if goagent runing
-hasGoagentRunning=False;
-for pid in psutil.pids():
-	try:
-		info = psutil.Process(pid)
-		if info.name()=='goagent.exe':
-			hasGoagentRunning=True
-			logging.warning('running goagent detected')
-			break
-	except:
-		pass
 
-#2.1 if not running then start local goagent 
-if hasGoagentRunning==False and goagentPath!="":
-	import subprocess
-	subprocess.Popen(goagentPath+"\\local\\goagent.exe")
+def startgae():
+    import subprocess
+    subprocess.Popen(goagentPath + "\\local\\goagent.exe")
 #3.check goagent update using proxy
 #3.1 visit goagent site via proxy
 def getGAE():
     proxy = urllib2.ProxyHandler({'http': '127.0.0.1:8087'})
     opener = urllib2.build_opener(proxy)
     urllib2.install_opener(opener)
-    link= urllib2.urlopen('https://goagent.googlecode.com/archive/3.0.zip')
+    link = urllib2.urlopen('https://goagent.googlecode.com/archive/3.0.zip')
     import cgi
     _, params = cgi.parse_header(link.headers.get('Content-Disposition', ''))
     filename = params['filename']
     import urllib
-    urllib.urlretrieve ('https://goagent.googlecode.com/archive/3.0.zip',filename)
+    urllib.urlretrieve('https://goagent.googlecode.com/archive/3.0.zip', filename)
+
+
 #print link.read()
 #3.2 get local goagent version
 def getLocalGAEVersion(path):
-    filepath = path+"\\local\\proxy.py"
-    versionlinePattern =re.compile("__version__ = \'.+\'")
+    filepath = path + "\\local\\proxy.py"
+    versionlinePattern = re.compile("__version__ = \'.+\'")
     with open(filepath) as file:
         for line in file:
-            if versionlinePattern.match(line)!=None:
+            if versionlinePattern.match(line) is not None:
                 return line
 
-	
+
 #3.3 get remote goagent version
 def getRemoteGAEVersion():
     return ""
+
+
 #3.4 if new version released goto update
 #4. do update
-def updateGAE(oldpath,newpath):
-    if checkGAERunning()!=False:
+def updateGAE(oldpath, newpath):
+    if checkGAERunning():
         info = checkGAERunning()
         info.kill()
-
     return
+
 
 def getGAEconf(path):
-    file=open(path+"//local//proxy.ini")
-
+    file = open(path + "//local//proxy.ini")
     return
+
+
 def checkGAERunning():
     for pid in psutil.pids():
-	try:
-		info = psutil.Process(pid)
-		if info.name()=='goagent.exe':
-			return info
-			break
-	except:
-		pass
+        try:
+            info = psutil.Process(pid)
+            if info.name() == 'goagent.exe':
+                return info
+                break
+        except:
+            pass
     return False
+
+
+#1.get local goagent path
+localpath = "C:\\Users\\Administrator"
+logging.warning('goagent path is ' + localpath)
+goagentPath = ""
+goagentFolderPattern = "goagent"
+pattern = re.compile(goagentFolderPattern)
+for filename in os.listdir(localpath):
+    if pattern.match(filename) is not None:
+        goagentPath = localpath + "\\" + filename
+        print  goagentPath
+
+#2.check if goagent runing
+hasGoagentRunning = checkGAERunning()
+
+#2.1 if not running then start local goagent
+if checkGAERunning() is False and goagentPath != "":
+    startgae()
+
+
 #4.1 download goagent package via proxy
 #4.2 extract downloaded package
 #4.3 get local goagent config info
